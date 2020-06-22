e98b3c16f6a4ca1897f03241d8a177383684ed0f
Katie King <kingofkaties@gmail.com>
2020-06-22 11:32:34 -0400

    Updates

---
+++ precache-manifest.6ca98dc16cfdbf2147a9a49df6e420da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c866fbbf2e9f2aad7bf8ca355dec48",
+    "url": "/katieKingProjectFive/index.html"
+  },
+  {
+    "revision": "a58564a5ee827bda365d",
+    "url": "/katieKingProjectFive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d23ecef2f04aca0417ad",
+    "url": "/katieKingProjectFive/static/js/2.be8439b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/katieKingProjectFive/static/js/2.be8439b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58564a5ee827bda365d",
+    "url": "/katieKingProjectFive/static/js/main.aeb3a4fa.chunk.js"
+  },
+  {
+    "revision": "5577555609d4624da755",
+    "url": "/katieKingProjectFive/static/js/runtime-main.0c10e89f.js"
+  }
+]);
